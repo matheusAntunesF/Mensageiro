1573b41ae8055cf46b06126ac9c533df4443d68e
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2025-07-20 15:57:11 -0300

    Criação do projeto

---
+++ src/main/java/io/github/matheusaantunesf/sms/SMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.matheusaantunesf.sms;
+
+/**
+ *
+ * @author Matheus Antunes <maf6@aluno.ifnmg.edu.br>
+ */
+public class SMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
